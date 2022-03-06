fee34a5fb86637b5b5462926e3b66e08ededce6c
Tom Humphries <tom.humph@gmail.com>
2022-03-06 14:19:16 +0000

    fix for github pages

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'docs')))
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
